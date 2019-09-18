8cd0f771155541bd16fb4ed9739a9fd7fdf0640e
Mark Sullivan <mark@sullivans.id.au>
2019-09-18 11:38:56 +1000

    Switch to abstract property

--- fmcapi/api_objects/data.py
+++ fmcapi/api_objects/data.py
@@ -1,47 +1,70 @@
 from unittest import mock
+from abc import ABCMeta, abstractmethod
 
 from fmcapi.api_objects.apiclasstemplate import APIClassTemplate
 
 
 class BaseData(object):
 
-    ATTRIBUTE = 'OVERRIDE_ME'
+    @property
+    @abstractmethod
+    def key(self):
+        pass
 
     def __init__(self, **kwargs):
-        print(f'Init for {self.ATTRIBUTE}')
+        print(f'Init for {self.key}')
         self.value = None
         self.parse_kwargs(**kwargs)
 
     def format_data(self):
-        print(f'Formatting data for {self.ATTRIBUTE}')
+        print(f'Formatting data for {self.key}')
         if self.value:
-            return {self.ATTRIBUTE: self.value}
+            return {self.key: self.value}
         return None
 
     def parse_kwargs(self, **kwargs):
-        print(f'Parsing kwargs for {self.ATTRIBUTE}')
-        if self.ATTRIBUTE in kwargs:
-            self.value = kwargs[self.ATTRIBUTE]
+        print(f'Parsing kwargs for {self.key}')
+        if self.key in kwargs:
+            self.value = kwargs[self.key]
 
 
 class Id(BaseData):
-    ATTRIBUTE = 'id'
+    @property
+    @abstractmethod
+    def key(self):
+        return 'id'
 
 
 class Name(BaseData):
-    ATTRIBUTE = 'name'
+    @property
+    @abstractmethod
+    def key(self):
+        return 'name'
 
 
 class Type(BaseData):
-    ATTRIBUTE = 'type'
+    @property
+    @abstractmethod
+    def key(self):
+        return 'type'
 
 
 class Objects(BaseData):
-    ATTRIBUTE = 'objects'
+    @property
+    @abstractmethod
+    def key(self):
+        return 'objects'
 
 
 class Literals(BaseData):
-    ATTRIBUTE = 'literals'
+    @property
+    @abstractmethod
+    def key(self):
+        return 'literals'
+
+
+class BaseAPI(object):
+    pass
 
 
 class NetworkGroups(APIClassTemplate):
@@ -53,12 +76,12 @@ class NetworkGroups(APIClassTemplate):
 
     REQUIRED_FOR_POST = ['name']
 
-    ATTRIBUTES = [Id, Name, Type, Objects, Literals]
+    KEYS = [Id, Name, Type, Objects, Literals]
 
     def __init__(self, fmc, **kwargs):
         super().__init__(fmc)
         self.type = 'NetworkGroup'
-        self._data = {data.ATTRIBUTE: data() for data in self.ATTRIBUTES}
+        self._data = {data.key: data() for data in self.KEYS}
         self.parse_kwargs(**kwargs)
 
     def parse_kwargs(self, **kwargs):
@@ -72,6 +95,7 @@ class NetworkGroups(APIClassTemplate):
             if value:
                 json_data.update(value)
         return json_data
+
 
 if __name__ == "__main__":
     d = {'id'      : 'my_id',
