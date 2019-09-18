cf45a0f035e09bbfd2c5b0b6106b9c39729d1753
Mark Sullivan <mark@sullivans.id.au>
2019-09-18 11:57:34 +1000

    Add hosts

--- fmcapi/api_objects/data.py
+++ fmcapi/api_objects/data.py
@@ -1,15 +1,14 @@
 from unittest import mock
 from abc import ABCMeta, abstractmethod
+from fmcapi.api_objects.helper_functions import get_networkaddress_type
 
-from fmcapi.api_objects.apiclasstemplate import APIClassTemplate
 
-
-class BaseData(object):
+class BaseData(metaclass=ABCMeta):
 
     @property
     @abstractmethod
     def key(self):
-        pass
+        return ''
 
     def __init__(self, **kwargs):
         print(f'Init for {self.key}')
@@ -30,58 +29,70 @@ class BaseData(object):
 
 class Id(BaseData):
     @property
-    @abstractmethod
     def key(self):
         return 'id'
 
 
 class Name(BaseData):
     @property
-    @abstractmethod
     def key(self):
         return 'name'
 
 
 class Type(BaseData):
     @property
-    @abstractmethod
     def key(self):
         return 'type'
 
 
 class Objects(BaseData):
     @property
-    @abstractmethod
     def key(self):
         return 'objects'
 
 
 class Literals(BaseData):
     @property
-    @abstractmethod
     def key(self):
         return 'literals'
 
 
-class BaseAPI(object):
-    pass
+class Value(BaseData):
+    @property
+    def key(self):
+        return 'value'
 
 
-class NetworkGroups(APIClassTemplate):
-    """
-    The NetworkGroups Object in the FMC.
-    """
+class Description(BaseData):
+    @property
+    def key(self):
+        return 'description'
 
-    URL_SUFFIX = '/object/networkgroups'
 
-    REQUIRED_FOR_POST = ['name']
+class BaseAPI(metaclass=ABCMeta):
 
-    KEYS = [Id, Name, Type, Objects, Literals]
+    @property
+    @abstractmethod
+    def url_suffix(self):
+        return ''
+
+    @property
+    @abstractmethod
+    def required_for_post(self):
+        return []
+
+    @property
+    @abstractmethod
+    def keys(self):
+        return []
+
+    @property
+    @abstractmethod
+    def type(self):
+        return ''
 
     def __init__(self, fmc, **kwargs):
-        super().__init__(fmc)
-        self.type = 'NetworkGroup'
-        self._data = {data.key: data() for data in self.KEYS}
+        self._data = {data().key: data() for data in self.keys}
         self.parse_kwargs(**kwargs)
 
     def parse_kwargs(self, **kwargs):
@@ -97,12 +108,66 @@ class NetworkGroups(APIClassTemplate):
         return json_data
 
 
+class NetworkGroups(BaseAPI):
+    """
+    The NetworkGroups Object in the FMC.
+    """
+
+    @property
+    def url_suffix(self):
+        return '/object/networkgroups'
+
+    @property
+    def required_for_post(self):
+        return ['name']
+
+    @property
+    def keys(self):
+        return [Id, Name, Type, Objects, Literals]
+
+    @property
+    def type(self):
+        return 'NetworkGroup'
+
+
+class Hosts(BaseAPI):
+    """
+    The Hosts Object in the FMC.
+    """
+
+    @property
+    def url_suffix(self):
+        return '/object/hosts'
+
+    @property
+    def required_for_post(self):
+        return ['id', 'name', 'value']
+
+    @property
+    def keys(self):
+        return [Id, Name, Value, Description]
+
+    @property
+    def type(self):
+        return get_networkaddress_type(self._data['value'].value)
+
+
 if __name__ == "__main__":
-    d = {'id'      : 'my_id',
-         'name'    : 'my_name',
-         'type'    : 'my_type',
-         'objects' : 'my_objects',
+
+    d = {'id': 'my_id',
+         'name': 'my_name',
+         'type': 'my_type',
+         'objects': 'my_objects',
          'literals': 'my_literals'}
 
     x = NetworkGroups(fmc=mock.Mock(), **d)
     print(x.format_data())
+
+    d = {'id': 'my_id',
+         'name': 'my_name',
+         'value': '10.0.0.1',
+         'description': 'my_description'}
+
+    x = Hosts(fmc=mock.Mock(), **d)
+    print(x.format_data())
+    print(x.type)
