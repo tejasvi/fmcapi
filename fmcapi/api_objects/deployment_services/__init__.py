74418fa05e85843141726f19013bc675e10540a6
Dax Mickelson <dmickels@cisco.com>
2021-05-27 16:50:42 -0600

    Placeholders for Deployments, PendingChanges, JobHistories, RollbackRequests Classes.

--- fmcapi/api_objects/deployment_services/__init__.py
+++ fmcapi/api_objects/deployment_services/__init__.py
@@ -3,7 +3,18 @@
 import logging
 from .deployabledevices import DeployableDevices
 from .deploymentrequests import DeploymentRequests
+from .deployments import Deployments
+from .pending_changes import PendingChanges
+from .job_histories import JobHistories
+from .rollback_requests import RollbackRequests
 
 logging.debug("In the deployment_services __init__.py file.")
 
-__all__ = ["DeployableDevices", "DeploymentRequests"]
+__all__ = [
+    "DeployableDevices",
+    "DeploymentRequests",
+    "Deployments",
+    "PendingChanges",
+    "JobHistories",
+    "RollbackRequests",
+]
