f70e9c0cf741d51ba38e508f85622d0f4a7cbecd
Dax Mickelson <dmickels@cisco.com>
2021-06-01 08:08:37 -0600

    Placeholders for Domain class in Status Services.

--- fmcapi/api_objects/system_information/__init__.py
+++ fmcapi/api_objects/system_information/__init__.py
@@ -2,7 +2,11 @@
 
 import logging
 from .serverversion import ServerVersion
+from .domain import Domain
 
 logging.debug("In the system_information __init__.py file.")
 
-__all__ = ["ServerVersion"]
+__all__ = [
+    "ServerVersion",
+    "Domain",
+]
--- fmcapi/api_objects/system_information/domain.py
+++ fmcapi/api_objects/system_information/domain.py
@@ -1,5 +1,25 @@
-"""
-Not yet implemented.
+"""Not yet implemented."""
 
-added FMC v6.5.0
-"""
+from fmcapi.api_objects.apiclasstemplate import APIClassTemplate
+import logging
+
+
+class Domain(APIClassTemplate):
+    """The Domain Object in the FMC."""
+
+    FIRST_SUPPORTED_FMC_VERSION = "6.5.0"
+
+    def post(self):
+        """POST method for API for Domain not supported."""
+        logging.info("POST method for API for Domain not supported.")
+        pass
+
+    def put(self):
+        """PUT method for API for Domain not supported."""
+        logging.info("PUT method for API for Domain not supported.")
+        pass
+
+    def delete(self):
+        """DELETE method for API for Domain not supported."""
+        logging.info("DELETE method for API for Domain not supported.")
+        pass
